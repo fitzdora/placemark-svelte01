ae738bd4282b70a335b436cfccad93b5a2920f29
Isadora Fitzgerald <fitzdora@gmail.com>
2023-05-20 18:21:25 +0100

    Install of axios

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,6 @@
     </div>
     <div class="column">
         <h1 class="title">Welcome to PlaceMark</h1>
-        <p>Vist heritage sites in County Cork</p>
+        <p>Organise and manage your heritage sites</p>
     </div>
 </div>
